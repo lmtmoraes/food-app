9f12de75bc57c5716050025d263069d4b02a15aa
Lucas Moraes <lmt.lucasmoraesteixeira@gmail.com>
2022-12-01 09:35:24 -0300

    nav host fragment adicionado à main activity

---
+++ app/src/main/java/com/example/food_app/ui/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.food_app.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import com.example.food_app.R
+import com.example.food_app.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navHostFragment: NavHostFragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(root) }
+
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.main_fragment) as NavHostFragment
+    }
+}
---
+++ app/src/main/java/com/example/food_app/ui/MainFragment.kt
@@ -0,0 +1,25 @@
+package com.example.food_app.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.food_app.R
+import com.example.food_app.databinding.FragmentMainBinding
+
+
+class MainFragment : Fragment() {
+
+    private var binding: FragmentMainBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+
+}
